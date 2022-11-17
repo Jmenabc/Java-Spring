7145e72e624fdfe4b15752994d755eabb0d99bbe
Jmenabc <menajavier2002@gmail.com>
2022-11-17 17:40:45 +0100

    TERMINADO, solo falta añadir la consulta al main y probar si funciona

--- src/main/java/com/ejemplo/jdbc/dao/structure/DAO/main.java
+++ src/main/java/com/ejemplo/jdbc/dao/structure/DAO/main.java
@@ -3,8 +3,13 @@ package com.ejemplo.jdbc.dao.structure.DAO;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// TODO ("INSERT INTO \"proyectoEclipse\".\"alumnos\" (id_alumno,nombre,apellidos,email)+ "VALUES(23,'Paco','Fernández','pf@altair.es')", conexionGenerada);"
+		//Creamos una instancia de la clase
+		EclToPostgreImpl consulta = new EclToPostgreImpl();
+		//Ejecutamos el insert
+		consulta.insertar("");
+		
 	}
 
 }
