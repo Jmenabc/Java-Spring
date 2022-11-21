a52e9be3769578a9f0834d29b9f09012651c3c78
DESKTOP-HGE6DGN\javi <csi22-jmennza@colegioaltair.net>
2022-11-21 11:34:42 +0100

    Terminada interfaz de consultasSQL

--- src/main/java/com/ejemplo/ORM/Mapper.java
+++ src/main/java/com/ejemplo/ORM/Mapper.java
@@ -2,15 +2,23 @@ package com.ejemplo.ORM;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 /*
- * 
+ * Interfaz de consultas SQl
  * @author Jmenabc
  */
 public interface Mapper {
-	
-	@Select("")
+
+	@Select("SELECT * FROM ejemploAlumno")
 	public List<Alumno> selectAllAlumnos();
-	
+
+	@Select("SELECT * FROM ejemploAlumno WHERE id = #{id}")
+	public Alumno selectAlumnoById(@Param("id") int id);
+
+	@Insert("INSERT INTO ejemploAlumno (alumno_nombre, alumno_apellido, alumno_apellido2) VALUES (#{alumno_nombre}, #{alumno_apellido}, #{alumno_apellido2})")
+	public int insertAlumno(Alumno alumno);
+
 }
